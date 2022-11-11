beb53278ee090567e0e2908768549399be11f9c5
Kratika Mishra <74831136+Kizziebasu@users.noreply.github.com>
2022-11-11 10:06:35 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+variable "hw" {
+  type = string
+  default = "t2.nano"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "myawsserver-krats-var" {
+  ami = "ami-09d3b3274b6c5d4aa"
+  instance_type = var.hw
+  tags = {
+    Name = "krats-aws-ec2-instance-Nomura v2"
+    Env = "test"
+    Owner = "kratika"
+  }
+}
