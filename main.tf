47e7c7b3f6bf62202b0438a7a8137af59bd405d8
Kratika Mishra <74831136+Kizziebasu@users.noreply.github.com>
2022-11-11 10:45:06 +0530

    chnaged server name

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "myawsserver-krats-var" {
   ami = "ami-09d3b3274b6c5d4aa"
   instance_type = var.hw
   tags = {
-    Name = "krats-aws-ec2-instance-Nomura v2"
+    Name = "krats-aws-ec2-instance-v2"
     Env = "test"
     Owner = "kratika"
   }
